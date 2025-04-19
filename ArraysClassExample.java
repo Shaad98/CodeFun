b51d86b8ff2fe4f1d65bb7748e225f0bab94e3a4
Shaad <141047163+Shaad98@users.noreply.github.com>
2025-04-19 10:41:07 +0530

    Basic things in python done

---
+++ ArraysClassExample.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class ArraysClassExample {
+   public static void main(String[] args) {
+     // int arr[] = {1,2,3};
+    //  int[] arr2 = {3,2,1};
+    // int [] arr3 = {1,2,3};
+    // System.out.println(arr3.length);
+    // int []arr4 = {1,2,3};
+
+    // int x[] = new int[5];
+    // System.out.println(Arrays.toString(x));
+
+    // int y[];
+    // y = new int[3];
+    // System.out.println(Arrays.toString(y));
+
+
+    // XXXXXXXXXXXXXX Not Valide XXXXXXX
+    // int z[];
+    // z = {1,2,3};
+    // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+    
+
+   }
+}
---
+++ main.py
@@ -0,0 +1,65 @@
+# name = "shaad bangi"
+# print(name.upper())
+# print(name.lower())
+# print(name[:5])
+
+# Each letter start become capital
+# print(name.title())
+# Only start of heading become capital
+# print(name.capitalize())
+
+# By default split by space
+# print(name.split())
+
+# l = ["Tony","Stark"]
+# fullName = " ".join(l)
+# print(fullName)
+
+# l1 = [3,2,5,1]
+
+# l2 = sorted(l1)
+# print(l2)
+
+# l3 = [5,2,8,3,1]
+# l3.sort()
+# print(l3)
+
+# x = (5,2,3,1,7)
+# XXXXXXXXXX Not work for tuple XXXXXXXXXXXX
+# x.sort()
+# print(x)
+# x = 2
+# x[3] = 20
+# XXXXXXXXXXXXXXXX XXXXXXXXXXXXXX
+# print(x)
+
+# It take it as tuple!
+# xyz = 1,2,3,4,5,4
+# print(type(xyz))
+# print(xyz)
+# print(len(xyz))
+# print(sum(xyz))
+# print(max(xyz))
+# print(min(xyz))
+
+# print(xyz.count(4))
+# print(xyz.index(4))
+
+# pqr = [8,2,4,4,5,1]
+# print(pqr.count(4))
+# print(pqr.index(4))
+# print(pqr.remove(2))
+# print(pqr)
+
+# num = 19
+# print(bin(num)[2:])
+# print(oct(num)[2:])
+# print(hex(num)[2:])
+
+# l = [n**2 for n in range(1,11) if n%5!=0]
+# print(l)
+
+# s = "Hello"
+# print(s.startswith("h"))
+# print(s.endswith("o"))
+
